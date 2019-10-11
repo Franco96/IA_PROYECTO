662157a40405f6a8c9cfa56b5b983791d462ce5f
Franco_96 <francolautarocarranza@hotmail.com>
2019-10-11 12:27:50 -0300

    Cambios

---
+++ AgentBDI-template/module_path_finding.pl
@@ -0,0 +1,29 @@
+:- encoding('iso_latin_1').
+:- module(path_finding,
+	  [
+	    buscar_plan_desplazamiento/3
+	  ]).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% buscar_plan_desplazamiento(+Metas, -Plan, -Destino)
+%
+
+
+buscar_plan_desplazamiento(_Metas, _Plan, _Destino).
+% << TODO: IMPLEMENTAR >>
+
+
+
+
+
+
+
+
+
+
+
+
+
